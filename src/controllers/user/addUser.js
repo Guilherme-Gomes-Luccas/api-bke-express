c4b931df7f06fd24f3031eac721a373e8412ec72
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2024-09-04 20:39:55 -0300

    validando user

--- src/controllers/user/addUser.js
+++ src/controllers/user/addUser.js
@@ -1,9 +1,19 @@
-import { create } from "../../models/userModel.js";
+import { create, validateUserToCreate } from "../../models/userModel.js";
 
 const addUser = async (req, res) =>{
     const user = req.body
 
-    const result = await create(user)
+    const userValidated = validateUserToCreate(user)
+    console.log(userValidated)
+
+    if (userValidated?.error) {
+        return res.status(400).json({
+            error: "Erro ao criar usuário, verifique os dados!",
+            fieldErrors: userValidated.error.flatten().fieldErrors
+        })
+    }
+
+    const result = await create(userValidated.data)
 
     if (!result) {
         return res.status(500).json({
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,6 +1,42 @@
 import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
 
 const prisma = new PrismaClient()
+
+const userSchema = z.object({
+    id: z.number({required_error: "O ID é obrigatória",
+        invalid_type_error: "O ID deve ser um número inteiro"
+    })
+        .positive({message: "O ID deve ser um número inteiro positivo"}),   
+    name: z.string({required_error: "O nome é obrigatória",
+        invalid_type_error: "O Nome deve ser uma string"
+    })
+        .min(3, {message: "O nome deve ter no mínimo 3 caracteres"})
+        .max(100, {message: "O nome deve ter no maxímo 100 caracteres"}),
+    email: z.string({
+        required_error: "O email é obrigatória",
+        invalid_type_error: "O email deve ser uma string"
+    })
+        .email({message: "O email deve ser um email valido"})
+        .max(200,  {message: "O email deve ter no maxímo 200 caracteres"}),
+    pass: z.string({
+        required_error: "A senha é obrigatória",
+        invalid_type_error: "A senha deve ser uma string"
+    })
+        .min(6, {message: "A senha deve ter no mínimo 6 caracteres"})
+        .max(256, {message: "A senha deve ter no maximo 256 caracteres"})
+})
+
+export const validateUser = (user) => {
+    return userSchema.safeParse(user)
+}
+
+export const validateUserToCreate = (user) => {
+    const partialUserSchema = userSchema.partial({
+        id: true
+    })
+    return partialUserSchema.safeParse(user)
+}
 
 export const getAll = async () => {
     const users = await prisma.user.findMany({
