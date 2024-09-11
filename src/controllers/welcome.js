b4be4d35b86e37ab98ab0ec6a1c2195686dc46f3
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2024-09-11 20:19:28 -0300

    adicionando middlewares

---
+++ src/controllers/welcome.js
@@ -0,0 +1,5 @@
+const welcome = (req, res) => {
+    res.json({message: "Bem vindo a API com express"})
+};
+
+export default welcome
--- src/server.js
+++ src/server.js
@@ -1,17 +1,24 @@
 import express from 'express'
 import userRouter from './routers/userRouter.js'
 import productRouter from './routers/productRouter.js'
+import logger from './controllers/middlewares/logger.js'
+import welcome from './controllers/welcome.js'
+import routeNotFounded from './controllers/routeNotFounded.js'
 import { PORT, HOST, ENVIRONMENT } from './config.js'
 
 const app = express()
 
+app.use(logger)
+
 app.use(express.json())
 
-app.get("/", (req, res) => {res.json({message: "Bem vindo a API"})})
+app.get("/", welcome)
 
 app.use('/user', userRouter)
 
 app.use('/product', productRouter)
+
+app.use('*', routeNotFounded)
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando no ambiente ${ENVIRONMENT} em ${ENVIRONMENT == 'production' ? HOST : HOST+':'+PORT}`)
